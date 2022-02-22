1741fdb09c3701f61cb13a00e19edbb828bdd22a
Igor Stepanov <stepanow.igor@gmail.com>
2022-02-22 15:14:01 +0400

    Update db.js

--- src/db.js
+++ src/db.js
@@ -32,58 +32,28 @@ export let users = {
 
 export let posts = {
     1: {
-        'user': 1,
-        'date': '13.08.2021 10:50',
-        'comment': 'Horizon Zero Dawn. Первый пост на сайте',
-        'content': [1, 2, 3]
-    },
-    2: {
-        'user': 1,
-        'date': '14.08.2021 16:22',
-        'comment': 'Horizon Zero Dawn. Скриншоты из игры',
-        'content': [4, 5]
-    },
-    3: {
         'user': 2,
         'date': '17.08.2021 21:59',
         'comment': 'Горы... Такие разные и такие впечатляющие...',
         'content': [6, 7, 8]
     },
-    4: {
+    2: {
         'user': 1,
         'date': '18.08.2021 3:48',
         'comment': 'Воробьёвы горы и ВДНХ',
         'content': [9, 10, 11, 12, 13, 14]
     },
-    5: {
+    3: {
         'user': 1,
         'date': '21.08.2021 12:25',
         'comment': 'Просто лягушка в море зелени ^^',
         'content': [15, 16, 17]
     },
-    6: {
+    4: {
         'user': 3,
         'date': '30.08.2021 17:42',
         'comment': 'Аквамариновый цвет',
         'content': [18, 19]
-    },
-    7: {
-        'user': 1,
-        'date': '02.09.2021 15:10',
-        'comment': 'Forza Horizon 4',
-        'content': [20, 21, 22, 23, 24, 25]
-    },
-    8: {
-        'user': 1,
-        'date': '24.09.2021 16:10',
-        'comment': 'Horizon Zero Dawn',
-        'content': [26, 27]
-    },
-    9: {
-        'user': 1,
-        'date': '03.10.2021 13:14',
-        'comment': 'Horizon Zero Dawn',
-        'content': [28, 29, 30, 31, 32, 33, 34, 35]
     }
 }
 
